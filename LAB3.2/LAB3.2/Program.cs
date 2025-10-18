33808a55de9d58b8835cf9d5f63a2af0a9e81073
Vitos <rustamminis@gmail.com>
2025-10-18 22:55:26 +0200

    Add files via upload

---
+++ LAB3.2/LAB3.2/Program.cs
@@ -0,0 +1,24 @@
+﻿try
+{
+    Console.WriteLine("Введите вещественное число А: ");
+    double A = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите целое число N (>0): ");
+    long N = long.Parse(Console.ReadLine());
+    double X = 1;
+    if (N > 0)
+    {
+        for (long i = 0; i < N; i++)
+        {
+            X *= A;
+        }
+        Console.WriteLine("A в степени N равно: "+X);
+    }
+    else
+    {
+        Console.WriteLine("N меньше или равно нулю, повторите попытку и введите число больше 0");
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
